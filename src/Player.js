95449ff230fd81b949136dc366bbe34a0e2d7c12
Nguyen Vinh Dinh <dinhdanvu@gmail.com>
2020-05-15 10:49:12 +0700

    update

--- src/Player.js
+++ src/Player.js
@@ -1,95 +1,32 @@
-import React, { Component } from 'react';
-import {
-  View
-} from 'react-native';
-import PropTypes from 'prop-types';
+import {State} from 'replate'
+import {makeState} from '../util'
+import {defaultTheme} from '../defaultTheme'
 
-import VideoWrapper from './VideoWrapper'
-import PlayerControls from './PlayerControls'
-import Overlay from './Overlay'
-import {connectVideo} from './connectVideo'
-import {actions} from './state'
-import styles from './styles'
-import {makeTheme} from './util'
-
-const videoPropsToSet = [
-  'source', 'rate', 'volume', 'muted', 'paused', 'resizeMode', 'repeat', 'playInBackground', 'name'
+const playerValues = [
+  {name:'source', initial: null},
+  {name:'buffering', initial: false},
+  {name:'error', initial: null},
+  {name:'ignoreSilentSwitch', initial: 'ignore'},
+  {name:'loaded', initial: false},
+  {name:'loading', initial: false},
+  {name:'metadata', initial: null},
+  {name:'muted', initial: false},
+  {name:'paused', initial: true},
+  {name:'playInBackground', initial: false},
+  {name:'playWhenInactive', initial: false},
+  {name:'progress', initial: null},
+  {name:'progressUpdateInterval', initial: 250},
+  {name:'rate', initial: 1.0},
+  {name:'repeat', initial: false},
+  {name:'resizeMode', initial: 'contain'},
+  {name:'volume', initial: 1.0},
+  {name:'duration', initial: null},
+  {name:'ref', initial: null},
+  {name:'ended', initial: false},
+  {name:'name', initial: null},
+  {name:'back', initial: null},
+  {name:'styles', initial: {}},
+  {name:'theme', initial: defaultTheme}
 ]
-const propsToWatch = [
-  'back', 'styles'
-]
 
-export class Player extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.setStateFromVideoProps()
-    props.theme && props.actions.theme(makeTheme(props.theme))
-  }
-
-  setStateFromVideoProps = () => {
-
-    videoPropsToSet.forEach((prop) => {
-      (typeof this.props.videoProps[prop] != 'undefined') && this.props.actions[prop](this.props.videoProps[prop])
-    })
-
-  }
-
-
-
-  componentWillReceiveProps(newProps) {
-    propsToWatch.forEach((prop) => {
-      if((typeof newProps[prop] != 'undefined') && newProps[prop] != this.props.player[prop]) {
-        this.props.actions[prop](newProps[prop])
-      }
-    })
-  }
-
-  render() {
-
-    return (
-      <View style={{flex: 1, width: '100%'}}>
-        <VideoWrapper videoProps={this.props.videoProps} />
-        <Overlay
-          fadeDuration={300}
-          displayDuration={3000}
-          forceVisible={this.props.player.paused || this.props.player.buffering}>
-          <PlayerControls layout={this.props.layout} />
-        </Overlay>
-      </View>
-    )
-  }
-}
-
-Player.defaultStyles = {
-  player: {
-    flex: 1
-  }
-}
-
-Player.propTypes = {
-  videoProps: PropTypes.shape({
-    name: PropTypes.string,
-    source: PropTypes.shape({
-      uri: PropTypes.string.isRequired
-    }).isRequired
-  }).isRequired,
-  back: PropTypes.func,
-  styles: PropTypes.object,
-  theme: PropTypes.object,
-  layout: PropTypes.shape({
-    Header: PropTypes.object,
-    Body: PropTypes.object,
-    Footer: PropTypes.object
-  })
-}
-
-export const Connected = connectVideo(['paused', 'buffering', 'theme'].concat(videoPropsToSet).concat(propsToWatch),
-  videoPropsToSet.concat(propsToWatch).reduce((result, prop) => {
-    result[prop] = actions[prop]
-    return result
-  }, {theme: actions.theme})
-)(Player)
-
-export default Connected
+export default makeState('player', playerValues)
